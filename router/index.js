05353a508317ce2d56ddb8d5474d473a4345c20c
Esteban Samaniego <107878186+MaxiSama12@users.noreply.github.com>
2025-06-20 00:21:53 -0300

    Se agregó el controller de clientes para obtener y registrar clientes, y se agregaron sus rutas que escuchan peticiones en index.js

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { getAllCliente, getClienteById, RegisterCliente } = require("../controller/cliente.controller");
 const { getAllRubros, createRubro } = require("../controller/rubro.controller")
 
 //rubro
@@ -7,7 +8,12 @@ router.post("/crear/rubro", createRubro);
 
 //comercio
 
+
+
 //cliente
-router.post("cliente", "...")
+router.get("/clientes", getAllCliente)
+router.get("/cliente/:id_cliente", getClienteById)
+router.post("/registro-cliente", RegisterCliente)
+
 
 module.exports = router;
