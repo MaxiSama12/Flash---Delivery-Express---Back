dbfb89660ea14929690c4217fb7d90a96decc1ad
Franco Giacobbe <frandg47@gmail.com>
2025-06-20 12:21:13 -0300

    funcion de crear producto en controlador

--- controller/producto.controller.js
+++ controller/producto.controller.js
@@ -61,32 +61,102 @@ const getAllProductosByComercio = async (req, res) => {
 };
 
 const getProductoById = async (req, res) => {
-  const { id } = req.params;
-  if (!id) {
-    return res.status(500).json({
-      mensaje: "No existe el producto",
-    });
-  }
-  const query = `SELECT * FROM producto WHERE id_producto = ?`;
-  db.query(query, [id], (err, result) => {
-    if (result.length === 0) {
-      return res.status(404).json({ mensaje: "Producto no encontrado" });
-    }
-
-    if (err) {
+  try {
+    const { id } = req.params;
+    if (!id) {
       return res.status(500).json({
-        mensaje: "Error al encontrar el producto",
+        mensaje: "No existe el producto",
       });
     }
-    return res.status(200).json({
-      mensaje: "Producto encontrado exitosamente",
-      producto: result,
+    const query = `SELECT * FROM producto WHERE id_producto = ?`;
+    db.query(query, [id], (err, result) => {
+      if (result.length === 0) {
+        return res.status(404).json({ mensaje: "Producto no encontrado" });
+      }
+
+      if (err) {
+        return res.status(500).json({
+          mensaje: "Error al encontrar el producto",
+          error: err,
+        });
+      }
+      return res.status(200).json({
+        mensaje: "Producto encontrado exitosamente",
+        producto: result,
+      });
     });
-  });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Error interno del servidor",
+      error: error.message,
+    });
+  }
+};
+
+const createProducto = async (req, res) => {
+  const {
+    nombre,
+    descripcion,
+    precio,
+    rating,
+    disponible,
+    id_comercio,
+    id_categoria,
+    url_imagen,
+  } = req.body;
+
+  try {
+    if (!nombre || !precio || !id_comercio || !id_categoria || !url_imagen) {
+      return res.status(400).json({
+        mensaje: "Faltan datos obligatorios",
+      });
+    }
+    const query = `INSERT INTO producto (
+    nombre,
+    descripcion,
+    precio,
+    rating,
+    disponible,
+    id_comercio,
+    id_categoria,
+    url_imagen,
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    db.query(
+      query,
+      [
+        nombre,
+        descripcion,
+        precio,
+        rating,
+        disponible,
+        id_comercio,
+        id_categoria,
+        url_imagen,
+      ],
+      (err, result) => {
+        if (err) {
+          return res.status(500).json({
+            mensaje: "Error al crear el producto",
+            error: err,
+          });
+        }
+        return res.status(201).json({
+          mensaje: "Producto creado exitosamente",
+          idProducto: result.insertId,
+        });
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Error interno del servidor",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = {
   getAllProductos,
   getAllProductosByComercio,
   getProductoById,
+  createProducto,
 };
