0e09184c2af92d883c3dfd8ebc1bb840ca0f7de3
GabrielTimo006 <gabitimo006@gmail.com>
2025-06-21 14:45:15 -0300

    Modificando la conexion

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Asegúrate de tener esto
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,       
-  user: process.env.USER_DB,       
-  password: process.env.PASS_DB,
-  database: process.env.NAME_DB
+  host: process.env.DB_HOST,       
+  user: process.env.DB_USER,       
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
